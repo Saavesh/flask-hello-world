e2711e12e9d9ba54f730f0662b55f3f0d53d2ed6
Sherry Saavedra <shsa7018@colorado.edu>
2024-11-03 01:44:11 -0800

    create route for db_select to insert data into basketball table

--- app.py
+++ app.py
@@ -47,3 +47,22 @@ def inserting():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Populated"
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgresql://first_database_ih70_user:IWeBo50iP8WmooP6q5ZMkSh579h8yGp3@dpg-csjijj5svqrc73esldfg-a/first_database_ih70")
+    cur = conn.cursor()
+    cur.execute('''
+    SELECT * FROM Basketball;
+    ''')        
+    records = cur.fetchall()
+    conn.close()
+    response_string = ""
+    response_string+='<table>'
+    for player in records:
+        response_string+='<tr>'
+        for info in player:
+            response_string+='<td>{}</td>'.format(info)
+        response_string+='</tr>'
+    response_string+='</table>'
+    return response_string
