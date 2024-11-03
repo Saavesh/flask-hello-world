177600c11b35fe1c08ec1946f79d05e54a441f48
Sherry Saavedra <shsa7018@colorado.edu>
2024-11-03 01:11:42 -0800

    troubleshooting 2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import psycopg2
 from flask import Flask
-# import psycopg2
+
 
 app = Flask(__name__)
 
@@ -7,8 +8,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Sherry Saavedra in 3308'
 
-# @app.route('/db_test')
-# def testing():
-#     conn = psycopg2.connect("postgresql://first_database_ih70_user:IWeBo50iP8WmooP6q5ZMkSh579h8yGp3@dpg-csjijj5svqrc73esldfg-a/first_database_ih70")
-#     conn.close()
-#     return "Database Connection Successful"
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://first_database_ih70_user:IWeBo50iP8WmooP6q5ZMkSh579h8yGp3@dpg-csjijj5svqrc73esldfg-a/first_database_ih70")
+    conn.close()
+    return "Database Connection Successful"
